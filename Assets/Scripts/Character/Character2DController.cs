330d05e423abbadee0af98dcb58d171dc7158c4c
Andrew Parkes <andrewparkesgunnell@gmail.com>
2021-03-31 20:09:56 +0100

    made jump while holding space

--- Assets/Scripts/Character/Character2DController.cs
+++ Assets/Scripts/Character/Character2DController.cs
@@ -10,6 +10,11 @@ public class Character2DController : MonoBehaviour
     [SerializeField]
     private float jumpForce = 8f;
 
+    [SerializeField]
+    private float counterJumpForce = 16f;
+
+    private Vector2 counterJump;
+
     private Rigidbody2D rb;
     public Animator animator;
     bool canMove = true;
@@ -17,21 +22,34 @@ public class Character2DController : MonoBehaviour
     float blockInputTime = 0;
 
     float movement;
-    bool jump = false;
+    bool jumpKeyHeld = false;
+    bool isJumping = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        counterJump = Vector2.down * counterJumpForce * rb.mass;
     }
 
     // Update is called once per frame
     void Update()
     {
-        movement = Input.GetAxis("Horizontal"); //left and right controll
-        if(!jump && Mathf.Abs(rb.velocity.y) < 0.001f) {
-            jump = Input.GetButtonDown("Jump");
-        }    
+        movement = Input.GetAxis("Horizontal"); //left and right controll) {
+        //jump = Input.GetButtonDown("Jump");   
+        if(Input.GetButtonDown("Jump"))
+        {
+            jumpKeyHeld = true;
+            if(Mathf.Abs(rb.velocity.y) < 0.001f)
+            {
+                isJumping = true;
+                rb.AddForce(Vector2.up * jumpForce * rb.mass, ForceMode2D.Impulse);
+            }
+        }
+        else if(Input.GetButtonUp("Jump"))
+        {
+            jumpKeyHeld = false;
+        }
     }
 
     void FixedUpdate()
@@ -47,10 +65,23 @@ public class Character2DController : MonoBehaviour
                 transform.rotation = movement < 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
             }
 
-            if(jump && Mathf.Abs(rb.velocity.y) < 0.001f) { // jump controll
+            /*if(jump && Mathf.Abs(rb.velocity.y) < 0.001f) { // jump controll
+            Debug.Log("Jump");
                 rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-                jump = false;
+            } else if (!jump && Mathf.Abs(rb.velocity.y) > 0.001f) {
+                Vector3 dampeningDirection = rb.velocity.normalized * -1.0f;
+                rb.AddForce( dampeningDirection * dampeningRate);
+                Debug.Log("Reduce Jump");
+            }*/
+
+            if(isJumping)
+            {
+                if(!jumpKeyHeld && Vector2.Dot(rb.velocity, Vector2.up) > 0)
+                {
+                    rb.AddForce(counterJump * rb.mass);
+                }
             }
+
             animator.SetFloat("XMovement", Mathf.Abs(movement));
         }
         animator.SetFloat("YMovement", rb.velocity.y);
